58b925e044cc8047a041efcb10830b5961bb54ec
B10809010 <tina96024@gmail.com>
2025-05-07 21:39:57 +0800

    feat: create NavBar

---
+++ components/NavBar.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="w-full h-full flex flex-col">
+    <div class="flex-grow">
+      <UNavigationMenu orientation="vertical" 
+        :items="mainNavItems" 
+        class="p-1 data-[orientation=vertical]:w-full"
+        :ui="{
+          link: 'text-2xl',
+        }"
+      >
+      <template #item="{ item, index, active }">
+        <div v-if="!item.children" class="w-full">
+          <div class="w-full flex items-center gap-x-1.5">
+            <UIcon v-if="item.icon" :name="item.icon" class="size-5" />
+            <div :class="item.icon ? '' : 'pl-5'">
+              {{ item.label }}
+            </div>
+          </div>
+        </div>
+      </template>
+      </UNavigationMenu>
+    </div>
+    <div class="flex-shrink-0 border-t border-[#B5B5B5]">
+      <UNavigationMenu orientation="vertical" 
+        :items="otherItems" 
+        class="p-1 data-[orientation=vertical]:w-full"
+        :ui="{
+          label: 'font-normal',
+          link: 'text-lg'
+        }"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const mainNavItems = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: '採購作業',
+      // icon: 'i-lucide-book-open',
+      active: true,
+      defaultOpen: true,
+      children: [
+        {
+          label: '請購單',
+          description: '',
+          icon: 'i-lucide-file-text',
+          to: '/Purchase/Requisition'
+        },
+        {
+          label: '進貨管理',
+          description: '',
+          icon: 'i-lucide-file-text',
+          to: '/Purchase/Order'
+        }
+      ]
+    },
+    {
+      label: '庫存管理',
+      children: []
+    },
+    {
+      label: '訂單管理',
+      icon: '',
+      // to: '/components',
+      defaultOpen: true,
+      children: []
+    },
+    {
+      label: '應收帳款管理',
+      icon: '',
+      // to: '/components',
+      defaultOpen: true,
+      children: []
+    },
+    {
+      label: '應付帳款管理',
+      icon: '',
+      defaultOpen: true,
+      children: []
+    },
+    {
+      label: '會計分錄',
+      icon: '',
+      defaultOpen: true,
+      to: '/Accounting/Entry',
+      children: [],
+    }
+  ]
+])
+const otherItems = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Settings',
+      type: 'label',
+    },
+    {
+      label: 'GitHub',
+      icon: 'i-simple-icons-github',
+      badge: '3.8k',
+      to: 'https://github.com/nuxt/ui',
+      target: '_blank'
+    },
+    {
+      label: 'Help',
+      icon: 'i-lucide-circle-help',
+      disabled: true
+    }
+  ]
+])
+</script>
