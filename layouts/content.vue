c8c61d6a78abcecbafa4eecff167e5cfb8ad83ce
B10809010 <tina96024@gmail.com>
2025-06-10 22:53:43 +0800

    feat: add search popover, handle read files and update data when click

--- layouts/content.vue
+++ layouts/content.vue
@@ -28,6 +28,53 @@
           @click="onDeleteData"
         />
       </div>
+      <div class="w-full flex items-center justify-center">
+        <CommonPopover>
+          <template #activator="{ open, close }">
+            <CommonInput
+              type="text"
+              placeholder="搜尋"
+              :value="queryText"
+              class="w-64"
+              @focus="open"
+            />
+          </template>
+          <template #default="{ close }">
+            <div class="flex flex-col gap-2">
+              <div class="flex items-center gap-2">
+                <CommonInput
+                  type="text"
+                  placeholder="搜尋編號"
+                  v-model="queryText"
+                />
+                <CommonButton
+                  label="清除搜尋"
+                  variant="outlined"
+                  @click="queryText = ''"
+                />
+              </div>
+              <div v-if="filterList?.length" class="w-full">
+                <table class="w-full">
+                  <thead class="*:px-2 text-xs font-normal text-dimmed">
+                    <!-- TODO: hardcoded columns for now -->
+                    <th class="text-left">編號</th>
+                    <th class="text-right">日期</th>
+                  </thead>
+                  <tbody>
+                    <tr v-for="item, lIdx in filterList.filter(list => list.title.includes(queryText))" :key="lIdx"  
+                      class="*:px-2 *:py-1.5 text-sm text-default hover:bg-surface-100 active:bg-surface-200 rounded-md cursor-pointer"
+                      @click.stop="onClickFilterItem(item.key)"
+                    >
+                      <td class="text-left">{{ item.title }}</td>
+                      <td class="text-right">{{ item.createDate }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </template>
+        </CommonPopover>
+      </div>
     </slot>
   </div>
   <div class="grow overflow-auto bg-surface px-6">
@@ -35,8 +82,17 @@
   </div>
 </template>
 <script setup lang="ts">
+const {
+  currentTabId,
+} = storeToRefs(useMainTabsStore());
 
-const { openFile } = useFiles();
+const {
+  viewModels,
+  getViewModel,
+  updateValue
+} = useViewModels();
+
+const { openFile, readFile } = useFiles();
 
 async function onAddNewBlank() {
 	try {
@@ -60,5 +116,30 @@ function onDeleteData() {
 async function onImportExcel() {
   const jsonData = await openFile();
   console.log(jsonData);
+}
+
+const filterList = ref<object[]>([])
+const queryText = ref('');
+
+onMounted(async () => {
+	const { data: result } = await useFetch('/api/readFiles/PO')
+  if (result.value) {
+    filterList.value = result.value.map((r) => {
+      return {
+        key: r.id.split('.')[0],
+        title: r.id.split('.')[0],
+        // TODO: orderDate should be changed to createDate 
+        createDate: r.orderDate
+      }
+    })
+  }
+});
+
+async function onClickFilterItem(id: string) {
+  if (!id) return
+  const result = await readFile(id);
+  console.log(result)
+  let viewModel = viewModels.value[currentTabId.value];
+  viewModel.values = result || {};
 }
 </script>
