21bb915b9f746a2c6603d9890baf54321cc342f6
B10809010 <tina96024@gmail.com>
2025-06-11 22:34:33 +0800

    fix: update get_base_path function to explicitly return PathBuf and remove unused serde imports in create_file

--- src-tauri/src/commands/get_base_path.rs
+++ src-tauri/src/commands/get_base_path.rs
@@ -4,7 +4,7 @@ use tauri::AppHandle;
 pub fn get_base_path(app: &AppHandle, folder: &str) -> PathBuf {
     #[cfg(debug_assertions)] // for dev, e.g. ../data/PO
     {
-        PathBuf::from("../data").join(folder)
+        return PathBuf::from("../data").join(folder);
     }
 
     #[cfg(not(debug_assertions))] // for bundled App 
@@ -12,6 +12,6 @@ pub fn get_base_path(app: &AppHandle, folder: &str) -> PathBuf {
         let mut path = tauri::api::path::app_data_dir(&app.config())
             .unwrap_or_else(|| PathBuf::from("."));
         path.push(folder);
-        path
+        return path;
     }
 }
