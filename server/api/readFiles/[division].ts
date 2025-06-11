d7dbb9246f5d2fa9593a649551432256dfd7a455
B10809010 <tina96024@gmail.com>
2025-06-11 20:59:39 +0800

    fix: move testing file to ../localDatas

--- server/api/readFiles/[division].ts
+++ server/api/readFiles/[division].ts
@@ -4,10 +4,10 @@ import path from 'path'
 export default defineEventHandler((event) => {
 	const division = event.context.params?.division
 
-  const dir = path.resolve(`data/${division}`)
+  const dir = path.resolve(`../localDatas/${division}`)
   const files = fs.readdirSync(dir)
   const data = files.map(file => {
-    console.log(file)
+    console.log('file:', file)
     const content = fs.readFileSync(path.join(dir, file), 'utf-8')
     return JSON.parse(content)
   })
