0a5d9dd4d15c1df27d9f077ac6c15936aac4538f
B10809010 <tina96024@gmail.com>
2025-06-12 09:11:32 +0800

    fix: add _ syntax to create a closure for error argument; and correct path in debug configuration for get_base_path function

--- src-tauri/src/commands/get_base_path.rs
+++ src-tauri/src/commands/get_base_path.rs
@@ -5,13 +5,13 @@ use tauri::{AppHandle, Manager};
 pub fn get_base_path(app: &AppHandle, folder: &str) -> PathBuf {
     #[cfg(debug_assertions)] // for dev, e.g. ../data/PO
     {
-        return PathBuf::from("../data").join(folder);
+        return PathBuf::from("../lacalDatas").join(folder);
     }
 
     #[cfg(not(debug_assertions))] // for bundled App 
     {
         let mut path = app.path().app_data_dir()
-            .unwrap_or_else(|| PathBuf::from("."));
+            .unwrap_or_else(|_| PathBuf::from("."));
         path.push(folder);
         return path;
     }
